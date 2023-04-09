b8671ada057b51488a26cea8ef64fef179df52ef
Aquiles-bb <brunoaquiles@ufpr.br>
2023-04-09 19:10:05 -0300

    Programa que lê um mapa a partir de um arquivo txt o colocando em uma matriz.

---
+++ carregaMapa.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char** cria_matriz(FILE* mapa, int* tam)
+{
+    int colunas, i = 0;
+    char** matriz;
+    fscanf(mapa, "%d %d", tam, &colunas);
+    colunas++;
+
+    matriz = malloc(sizeof(char*)*(*tam));
+    while (i < (*tam)){
+        matriz[i] = malloc(sizeof(char)*colunas+1);
+        i++;
+    }
+    i = 0;
+
+    fgets(matriz[i], colunas, mapa);
+    while(i < (*tam)){
+        fgets(matriz[i], colunas+1, mapa);
+        matriz[i][strcspn(matriz[i], "\n")] = '\0';
+        i++;
+    }
+
+    return matriz;
+}
+
+void imprime_matriz(char** matriz, int tam)
+{
+    for (int i = 0; i < tam; i++)
+        printf ("%s\n", matriz[i]);
+}
+
+int main(){
+    FILE* mapa = fopen("map.txt", "r");
+    int tam;
+
+    char** matriz = cria_matriz(mapa, &tam);
+    imprime_matriz(matriz, tam);
+
+    return 0;
+}
--- makefile
+++ makefile
@@ -1,11 +1,17 @@
 manipula: manipula.c
 	gcc manipula.c -o manipula -g -Wall -std=c99
 
+carregaMapa: carregaMapa.c
+	gcc carregaMapa.c -o carregaMapa -g -Wall -std=c99
+
+crun: carregaMapa
+	./carregaMapa
+
 run: manipula 
 	./manipula
 
 clean:
-	rm manipula
+	rm manipula carregaMapa
 
 vrun: manipula
 	valgrind --leak-check=full ./manipula
