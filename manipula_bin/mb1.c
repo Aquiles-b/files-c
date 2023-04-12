ae42503693dd10ad8beb1c269aa00fa4b9efdff1
Aquiles-bb <brunoaquiles@ufpr.br>
2023-04-11 22:38:51 -0300

    Exercicios com arquivos binários

---
+++ manipula_bin/mb1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Retorna um numero aleatorio.*/
+long longAleat()
+{
+    return (rand() * rand());
+}
+
+void escreveNumsAleat(FILE *arq)
+{
+    int n = rand() + 101;
+    long *v = malloc(sizeof(long)*n);
+    for (int i = 0; i < n; i++) {
+        v[i] = longAleat();
+    }
+    fwrite(v, sizeof(long), n, arq);
+
+    free(v);
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    FILE *arq = fopen("longsAleatorios", "w");
+    if (!arq) {
+        perror("Erro ao criar arquivo.");
+        return 0;
+    }
+
+    escreveNumsAleat(arq);
+    fclose(arq);
+
+    return 0;
+}
